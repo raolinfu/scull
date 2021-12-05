48bd52c71ffcdf499d21fc10d8f03ee9422fb399
raolinhu <1145333729@qq.com>
2021-12-05 12:58:36 +0800

    framework

--- helloworld.c
+++ helloworld.c
@@ -1,14 +1,35 @@
-#include <linux/init.h>
+#include<linux/init.h>
+#include<linux/slab.h>		/* kmalloc() */
+#include<linux/types.h>
 #include<linux/fs.h>
 #include<linux/printk.h>
 #include<linux/cdev.h>
 #include<linux/semaphore.h>
-#include <linux/kernel.h>
-#include <linux/module.h>
+#include<linux/kernel.h>
+#include<linux/module.h>
 #include"scull.h"
 
-int scull_major;
-int scull_minor;
+int scull_major =   SCULL_MAJOR;
+int scull_minor =   0;
+int scull_nr_devs = SCULL_NR_DEVS;	/* number of bare scull devices */
+int scull_quantum = SCULL_QUANTUM;
+int scull_qset =    SCULL_QSET;
+struct scull_dev *scull_devices;	/* allocated in scull_init_module */
+
+struct scull_qset{
+	void **data;
+	struct scull_qset *next;
+};
+struct scull_dev{
+	struct scull_qset *data;
+	int quantum;
+	int qset;
+	unsigned long size;
+	unsigned int access_key;
+	struct semaphore sem;
+	struct cdev cdev;
+};
+
 loff_t scull_llseek(struct file * filp, loff_t off, int b){
 	return off;
 }
@@ -21,12 +42,32 @@ ssize_t  scull_write(struct file *filp, const char __user *buff, size_t count, l
 int scull_ioctl(struct inode *inode, struct file *flip, unsigned int cmd, unsigned long b){
 	return 0;
 }
+int scull_trim(struct scull_dev *dev){
+	struct scull_qset *next, *dptr;
+	int qset = dev->qset;
+	int i;
+	for(dptr = dev->data; dptr; dptr = next){
+		if(dptr->data){
+			for(i = 0; i < qset; i++)
+				kfree(dptr->data[i]);
+			kfree(dptr->next);
+			dptr->data = NULL;
+		}
+		next = dptr->next;
+		kfree(dptr);
+	}
+	dev->size = 0;
+	// dev->quantum = scull_quantum;
+	// dev->qset = scull_qset;
+	dev->data = NULL;
+	return 0;
+}
 int scull_open(struct inode *inode, struct file *flip){
 	struct scull_dev *dev;
 	dev = container_of(inode->i_cdev, struct scull_dev, cdev);
 	flip->private_data = dev;
 	if((flip->f_flags & O_ACCMODE) == O_WRONLY){
-		
+		scull_trim(dev);		
 	}
 	return 0;
 }
@@ -53,6 +94,51 @@ static void scull_setup_dev(struct scull_dev *dev, int index){
 }
 static int __init lkm_init(void)
 {
+	int result, i;
+	dev_t dev = 0;
+
+	if (scull_major) {
+		dev = MKDEV(scull_major, scull_minor);
+		result = register_chrdev_region(dev, scull_nr_devs, "scull");
+	} else {
+		result = alloc_chrdev_region(&dev, scull_minor, scull_nr_devs,
+				"scull");
+		scull_major = MAJOR(dev);
+	}
+	if (result < 0) {
+		printk(KERN_WARNING "scull: can't get major %d\n", scull_major);
+		return result;
+	}
+
+	scull_devices = kmalloc(scull_nr_devs * sizeof(struct scull_dev), GFP_KERNEL);
+	if (!scull_devices) {
+		result = -ENOMEM;
+		goto fail;  /* Make this more graceful */
+	}
+	memset(scull_devices, 0, scull_nr_devs * sizeof(struct scull_dev));
+
+        /* Initialize each device. */
+	for (i = 0; i < scull_nr_devs; i++) {
+		scull_devices[i].quantum = scull_quantum;
+		scull_devices[i].qset = scull_qset;
+		mutex_init(&scull_devices[i].lock);
+		scull_setup_cdev(&scull_devices[i], i);
+	}
+
+        /* At this point call the init function for any friend device */
+	dev = MKDEV(scull_major, scull_minor + scull_nr_devs);
+	dev += scull_p_init(dev);
+	dev += scull_access_init(dev);
+
+#ifdef SCULL_DEBUG /* only when debugging */
+	scull_create_proc();
+#endif
+
+	return 0; /* succeed */
+
+  fail:
+	scull_cleanup_module();
+	return result;
 	printk("Hello World\n");
 	return 0;
 }
--- scull.h
+++ scull.h
@@ -1,17 +1,27 @@
-#include<linux/cdev.h>
-#include<linux/semaphore.h>
+#ifndef SCULL_MAJOR
+#define SCULL_MAJOR 0   /* dynamic major by default */
+#endif
+#ifndef SCULL_NR_DEVS
+#define SCULL_NR_DEVS 4    /* scull0 through scull3 */
+#endif
 
-struct scull_qset{
-	void **data;
-	struct scull_qset *next;
-};
+#ifndef SCULL_P_NR_DEVS
+#define SCULL_P_NR_DEVS 4  /* scullpipe0 through scullpipe3 */
+#endif
 
-struct scull_dev{
-	struct scull_qset *data;
-	int quantum;
-	int qset;
-	unsigned long size;
-	unsigned int access_key;
-	struct semaphore sem;
-	struct cdev cdev;
-};
+/*
+ * The bare device is a variable-length region of memory.
+ * Use a linked list of indirect blocks.
+ *
+ * "scull_dev->data" points to an array of pointers, each
+ * pointer refers to a memory area of SCULL_QUANTUM bytes.
+ *
+ * The array (quantum-set) is SCULL_QSET long.
+ */
+#ifndef SCULL_QUANTUM
+#define SCULL_QUANTUM 4000
+#endif
+
+#ifndef SCULL_QSET
+#define SCULL_QSET    1000
+#endif
